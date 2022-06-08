3b21d0d84aa8a48593680d6efca063ecf246f1fb
Bukolads <oladapobukola35@yahoo.com>
2022-06-07 22:55:14 -0700

    print 98 fibonacci nos

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,11 +24,11 @@ a_head = a / 1000000000; /* break larger num into 2 parts */
 a_tail = a % 1000000000;
 b_head = b / 1000000000;
 b_tail = b % 1000000000;
-for(; counter < 99; counter++)
+for (; counter < 99; counter++)
 {
-overflow = (a_tail + b_tail) / 1000000000;
-sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
-sum_head = (a_head + b_head) + overflow;
+overflow = (a_tail+b_tail) / 1000000000;
+sum_tail = (a_tail+b_tail)-(1000000000 * overflow);
+sum_head = (a_head+b_head)+overflow;
 printf(", %lu%lu", sum_head, sum_tail);
 a_head = b_head;
 a_tail = b_tail;
