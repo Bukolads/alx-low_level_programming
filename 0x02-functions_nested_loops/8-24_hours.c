9a281dfb419575f69fb41797e575824ba860e45e
Bukolads <oladapobukola35@yahoo.com>
2022-06-07 21:55:06 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,29 +16,25 @@ while (h2 <= 2)
 {
 	if (h2 == 2)
 	{
-		/*Restrain to 23h, not 29*/
-		a = 3;
+	/*Restrain to 23h, not 29*/
+	a = 3;
 	}
-
 	h1 = 0;
-
 	while (h1 <= a)
 	{
-		m2 = 0;
-	
-		while (m2 <= 5)
+	m2 = 0;
+	while (m2 <= 5)
 		{
-			m1 = 0;
-
+		m1 = 0;
 			while (m1 <= 9)
 			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
+			_putchar('0' + h2);
+			_putchar('0' + h1);
+			_putchar(':');
+			_putchar('0' + m2);
+			_putchar('0' + m1);
+			_putchar('\n');
+			m1++;
 			}
 			m2++;
 		}
